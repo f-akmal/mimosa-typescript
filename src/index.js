e43129b3ce7b927baf78fbf9611f1b895553e4fc
unknown <AKMAL@KULLT12012.verit.dnv.com>
2015-05-01 22:41:51 +0800

    this one works

--- src/index.js
+++ src/index.js
@@ -1,119 +1,53 @@
+/// <reference path="../typings/typescript/typescript.d.ts" />
+/// <reference path="../typings/node/node.d.ts" />
+
 "use strict";
 
 var fs = require( "fs" )
   , path = require( "path" )
   , config = require( "./config" )
-  , ts
-  , libSnapshot
-  , lib
-  , settings
-  , scriptDetect
   , getExtensions = function ( mimosaConfig ) {
     return mimosaConfig.typescript.extensions;
   };
 
-var _tsSettings = function( tsOptions ) {
-  var st = new ts.CompilationSettings();
-
-  st.codeGenTarget = tsOptions.codeGenTarget;
-  st.moduleGenTarget = tsOptions.moduleGenTarget;
-  st.mapSourceFiles = tsOptions.mapSourceFiles;
-  st.gatherDiagnostics = true;
-  return ts.ImmutableCompilationSettings.fromCompilationSettings( st );
-};
-
-var _setup = function( tsOptions ) {
-  ts = require( "typescript-api" );
-  scriptDetect = new RegExp( ts.tripleSlashReferenceRegExp.source, "gm" );
-  settings = _tsSettings( tsOptions );
-  lib = path.resolve( require.resolve( "typescript" ), "../lib.d.ts" );
-  libSnapshot = ts.ScriptSnapshot.fromString( fs.readFileSync( lib, "utf-8" ) );
-};
-
-var _compileTs = function ( file, data ) {
-  var compiler = new ts.TypeScriptCompiler( new ts.NullLogger(), settings );
-  compiler.addFile( lib, libSnapshot );
-
-  data.replace( scriptDetect, function(match, p1, p2, filename) {
-    var fullFilePath = path.join( path.dirname( file) , filename );
-    var text = fs.readFileSync( fullFilePath, "utf8" );
-    var importSnap = ts.ScriptSnapshot.fromString( text );
-    compiler.addFile( filename, importSnap );
-  });
-
-  var snapshot = ts.ScriptSnapshot.fromString( data );
-  compiler.addFile( file, snapshot );
-
-  var it = compiler.compile()
-    , result
-    , current
-    , error
-    , output
-    , map;
-
-  while (it.moveNext()) {
-    result = it.current();
-
-    for (var i = 0; i < result.diagnostics.length; i++) {
-      var diagnostic = result.diagnostics[i];
-      if ( !error ) {
-        error = "";
-      }
-      error += diagnostic.fileName() + ": " + diagnostic.line() + ": " + diagnostic.character() + ": " + diagnostic.message();
-      var badLine = data.split( "\n" )[diagnostic.line()];
-      error += "\n" + badLine + "\n";
-      error += new Array( diagnostic.character() + 1 ).join( " " ) + "^\n\n";
-    }
-
-    var reFileName = file.replace( path.extname(file), "") + ".js";
-    for (var k = 0; k < result.outputFiles.length; k++) {
-      current = result.outputFiles[k];
-      if ( !current ) {
-        continue;
-      }
-      var name = current.name;
-      if ( name === reFileName ) {
-        output = current.text;
-      }
-      if ( name === reFileName + ".map" ) {
-        map = current.text;
-      }
-    }
-  }
-
-  // let mimosa core handle source maps, strip it out
-  if ( map ) {
-    output = output.split("\n").map( function( line ) {
-      return line.replace(/^\/\/# [A-Za-z0-9\.=_-]+/, "");
-    }).join("\n");
-  }
-
-  return {
-    error: error,
-    text: output,
-    map: map
-  };
-};
-
-var compile = function ( mimosaConfig, file, cb ) {
-  if ( !ts ) {
-    _setup( mimosaConfig.typescript.options );
-  }
-
-  if (
-    mimosaConfig.typescript.compileVendorDefinitionFiles
-    ||
-    !/\.d\.ts$/.test(file.inputFileName)
-    ||
-    ( file.isVendor === false ||
-      file.isVendor === undefined )
-  ) {
-    var result = _compileTs( file.inputFileName, file.inputFileText );
-    cb( result.error, result.text, mimosaConfig.typescript, result.map );
-  } else {
-    // vendor file,
-    cb( undefined, undefined, mimosaConfig.typescript, undefined );
-  }
+var compile = function(mimosaConfig, file, cb){
+	if (file.isVendor){return cb();}
+	
+	var ts = require('typescript');
+	var outputs = {};
+	var options = mimosaConfig.typescript.options;
+	
+	var compilerOptions = { 
+		noEmitOnError: true,
+		noImplicitAny: true,
+		target: options.codeGenTarget,
+		module: options.moduleGenTarget,
+	};
+	
+	var host = ts.createCompilerHost(compilerOptions);
+	host.writeFile = function (name, text, writeByteOrderMark) {
+    outputs = text;
+  	};
+	
+	var program = ts.createProgram([file.inputFileName], compilerOptions, host);
+	var emitResult = program.emit();
+	
+	var allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+	
+	var errors = allDiagnostics.map(function(diagnostic){
+		var lineAndCharacter = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+		var line = lineAndCharacter.line + 1;
+		var character = lineAndCharacter.character + 1;
+		var fName = path.basename(file.inputFileName);
+//		return fName + '(' + line + ',' + character + '): TS' + diagnostic.code + ': ' + diagnostic.messageText + '\n';
+		return diagnostic.messageText + '\n';
+	});
+	
+	if (errors.length) {
+		cb(errors);
+	} else {
+		cb(undefined, outputs);
+	}
 };
 
 module.exports = {
---
+++ typings/tsd.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="typescript/typescript.d.ts" />
